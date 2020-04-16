fed99e450239435ece393033a8e9c53df2ce6950
Regina Furness <skritschy@gmail.com>
2020-04-16 15:43:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-def my_collect(collection)
-  i = 0
-  new_collection = []
-  while i > collection.length
-  new_collection << yield(collection[i])
-  i -= 1
-  end
-  new_collection.join(" ").split
-=======
 def my_collection(array)
   i = 0
   collection = []
