17095fb59ff5e1b8ef845b6ed2e7b249275a29e1
Regina Furness <skritschy@gmail.com>
2020-04-16 15:51:24 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,22 @@
+
 def my_collect(collection)
-  i = collection.length
-  n = 0
+  i = 0
   new_collection = []
-  while i>0
-  new_collection << yield(collection[n])
-  n += 1
-  i -= 1
+  while i < collection.length
+  new_collection << yield(collection[i])
+  i += 1
   end
-  new_collection.join(" ").split
+ new_collection
+end
+
+
+def my_collection(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
 
